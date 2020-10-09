f9d459cb248138b12c9d4299e4f077f19e460c6a
Павел Пальчак <pavel-palchak@yandex.ru>
2020-10-09 16:26:17 +0300

    Главная перенесена в index.

--- GoodWill-Site-frontend/pages/index.vue
+++ GoodWill-Site-frontend/pages/index.vue
@@ -1,12 +1,85 @@
 <template>
-  <v-card flat height="88vh">
-    <h1>Index</h1>
-
-
+  <v-card class="homeWrap" flat>
+    <v-container>
+      <slider/>
+    </v-container>
+    <router-link class="linkNewsDetailed" :to="newsDetailed.href">
+      <v-row
+        align="start" justify="center">
+        <v-col
+          cols="10" md="5"
+          v-for="newItem of newsArr.slice(0,6)"
+          :key="newItem.id"
+          class="newsCard">
+          <h4> {{ newItem.heading }} </h4>
+          <v-banner class="cardDescription">
+            {{ newItem.title }}
+          </v-banner>
+        </v-col>
+      </v-row>
+    </router-link>
   </v-card>
 </template>
 
 <script>
+import slider from "~/components/slider.vue";
 
-
+export default {
+  name: "home",
+  data() {
+    return {
+      newsArr: [],
+      newsDetailed: {
+        href: '/news/_id',
+      },
+      title: 'Главная',
+      description: 'Главная описание'
+    }
+  },
+  head() {
+    return {
+      title: this.title,
+      meta: [
+        {
+          hid: 'Хид Главная',
+          name: 'Имя Главная',
+          content: this.description
+        }
+      ]
+    }
+  },
+  async created()
+  {
+    try {
+      const res = await this.$axios.get('http://localhost:3004/news')
+      this.newsArr = res.data;
+    } catch (e) {
+      console.error(e);
+    }
+  },
+  components: {
+    slider,
+  }
+}
 </script>
+
+<style lang="scss">
+.homeWrap {
+  @include size-window;
+
+  .linkNewsDetailed {
+    text-decoration: none;
+
+    .newsCard {
+      color: $colorCorporate;
+
+      .cardDescription {
+        margin: 5px;
+      }
+
+      margin: 0 auto;
+      padding: 5px;
+    }
+  }
+}
+</style>
